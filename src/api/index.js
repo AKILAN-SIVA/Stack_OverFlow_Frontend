f67dfdde06783aabb0775fdbfeecdabfdd04213f
Akilan <akilanlakshmi2003@gmail.com>
2023-11-14 18:18:39 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API = axios.create({ baseURL: 'https://stackoverflow-jatk.onrender.com/' })
+const API = axios.create({ baseURL: 'https://stackoverflow-r1wz.onrender.com/' })
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("Profile")) {
